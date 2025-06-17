f6e3a8884eb43e8db26f165010adc0a6f8cf6124
osama_abdellatif <155384353+MAD-SAM22@users.noreply.github.com>
2025-06-17 22:44:08 +0300

    VideoPlayer component added to the About page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Service from "./routes/Service";
 import Contact from "./routes/Contact";
+import VideoPlayer from './Componenets/VideoPlayer/VideoPlayer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About setPlayState={setPlayState} />} />
           <Route path="/service" element={<Service />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <VideoPlayer playState={playState} setPlayState={setPlayState} />
       </Router>
     </div>
   );
